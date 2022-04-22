a0f9ed4934778cc18d882e1080528a9f52870de8
Douglas Nickson <douglas.nickson1@gmail.com>
2022-04-21 23:44:28 -0300

    feat: search stock data

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,22 @@
 module.exports = {
   presets: [
     ['@babel/preset-env', { targets: { node: 'current' } }],
-    '@babel/preset-typescript'
+    '@babel/preset-typescript',
   ],
   plugins: [
-    ['module-resolver', {
-      alias: {
-        "@modules": "./src/modules",
-        "@config": "./src/config",
-        "@services": "./src/services",
-        "@shared": "./src/shared"
-      }
-    }],
-    "babel-plugin-transform-typescript-metadata",
-    ["@babel/plugin-proposal-decorators", { "legacy": true }],
-    ["@babel/plugin-proposal-class-properties", { "loose": true }],
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@modules': './src/modules',
+          '@config': './src/config',
+          '@services': './src/services',
+          '@shared': './src/shared',
+        },
+      },
+    ],
+    'babel-plugin-transform-typescript-metadata',
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
   ],
-}
+};
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,10 +1,16 @@
+import 'reflect-metadata';
+import 'dotenv/config';
+
 import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+
+import '@shared/container';
 
 const app = express();
-
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' });
-});
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('Server started on port 3333! 🏆');
